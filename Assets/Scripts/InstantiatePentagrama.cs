f8e4e782ec84dd410f3cf0338bc58d32c352f21f
FranciscoCai <jiehaocai54@gmail.com>
2025-04-05 11:19:22 +0200

    asd

---
+++ Assets/Scripts/InstantiatePentagrama.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using UnityEngine;
+
+public class InstantiatePentagrama : MonoBehaviour
+{
+    [SerializeField] private GameObject[] piezasPentagrama;
+    private int numbInstance;
+    [SerializeField] private float cd;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Beluga"))
+        {
+            StartCoroutine(PentagramaInstantiate());
+        }
+    }
+
+    // Update is called once per frame
+    private IEnumerator PentagramaInstantiate()
+    {
+        while (true)
+        {
+            piezasPentagrama[numbInstance].SetActive(true);
+            numbInstance++;
+            if(numbInstance >= piezasPentagrama.Length)
+            {
+                break;
+            }
+            yield return new WaitForSeconds(cd);
+        }
+    }
+}
